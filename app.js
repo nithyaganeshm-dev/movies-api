fecf29e991f4b57d09e4ba8e31aa4457d68fa9f8
nithyaganeshm-dev <mnithyaganesh@gmail.com>
2024-09-23 21:31:42 +0530

    feat: files modified

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import router from "./routes/movies.route.js";
+import connectDB from "./lib/db.js";
 const port = 3000;
 
 const app = express();
+
+// connectDB
+connectDB()
 
 // CRUD Operations
 app.use("/movies", router);
